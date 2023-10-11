66bba91dd0f1f89899fbce77881a5c86815d9f26
tiffanytiho <135434318+tiffanytiho@users.noreply.github.com>
2023-10-11 01:53:49 +0000

    CSS
    Mes débuts en CSS

---
+++ exo1.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>
+		
+	</title>
+	<link href="design1.css" rel="stylesheet">
+</head>
+<body>
+	<main>
+		<div class="conteneur">
+			<div class="interieur">
+				<div class="box"> </div>
+				<div class="box"> </div>
+				<div class="box"> </div>
+			</div>
+		</div>
+	</main>
+</body>
+</html>
